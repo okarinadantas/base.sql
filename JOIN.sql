7abbb67a92b4d85eab683aa668b1bd4282d19243
Karina Dantas Luiz <karinadantasluiz@gmail.com>
2021-07-28 14:28:01 -0300

    Delete JOIN.sql

--- JOIN.sql
+++
@@ -1,7 +0,0 @@
-USE ADVENTUREWORKS2019
-
-SELECT
-P.NameStyle, P.LastName, P.BusinessEntityID, EA.EmailAddress
-FROM PERSON.Person P
-JOIN PERSON.EmailAddress EA 
-	ON EA.BusinessEntityID = P.BusinessEntityID
